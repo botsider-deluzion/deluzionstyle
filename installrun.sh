4bfba9f96b4a959109c4306d7e2c294628d1c15b
botsider-deluzion <119793028+botsider-deluzion@users.noreply.github.com>
2022-12-04 21:24:16 +0700

    Create installrun.sh

---
+++ installrun.sh
@@ -0,0 +1,17 @@
+#### Please run on "Super Domain" ####
+
+# Update Linux Package
+apt update -y && 
+apt upgrade -y && 
+apt install update -y && 
+apt install upgrade -y && 
+
+#Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && 
+sudo dpkg -i google-chrome-stable_current_amd64.deb && 
+
+#Install Python
+apt install python &&
+pip install -r piprequirements.txt && 
+
+#Install NoMachine
