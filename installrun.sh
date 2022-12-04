c2aa3a1a3f23f2533ea388880477629c1cf112ed
botsider-deluzion <119793028+botsider-deluzion@users.noreply.github.com>
2022-12-04 21:36:27 +0700

    Update installrun.sh

--- installrun.sh
+++ installrun.sh
@@ -6,6 +6,11 @@ apt upgrade -y &&
 apt install update -y && 
 apt install upgrade -y && 
 
+#Mandatory
+apt install wget && 
+apt install curl && 
+apt install git && 
+
 #Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && 
 sudo dpkg -i google-chrome-stable_current_amd64.deb && 
