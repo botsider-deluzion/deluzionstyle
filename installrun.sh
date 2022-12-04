91350f5693e8ba9635c03c51b82b227c97e6019f
botsider-deluzion <119793028+botsider-deluzion@users.noreply.github.com>
2022-12-04 22:47:07 +0700

    Update installrun.sh

--- installrun.sh
+++ installrun.sh
@@ -11,11 +11,18 @@ apt install upgrade -y &&
 apt install wget && 
 apt install curl && 
 apt install git && 
+apt install proot && 
+apt install neofetch && 
+apt install htop && 
 
 #Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && 
-sudo dpkg -i google-chrome-stable_current_amd64.deb && 
+dpkg -i google-chrome-stable_current_amd64.deb && 
 
 #Install Python
 apt install python &&
-pip install -r piprequirements.txt
+pip install -r piprequirements.txt && 
+
+#Install NoMachine
+wget https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb && 
+dpkg -i nomachine_7.10.1_1_amd64.deb
